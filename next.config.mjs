70c3b819344953bbbadd92e48104309f9a7d92ec
ojeandev <ojeanrodriguesdev@gmail.com>
2024-10-06 04:42:55 +0200

    Fix Build Error: 4.1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks.cacheGroups = {
+        framework: {
+          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+          name: 'framework',
+          chunks: 'all',
+          priority: 40,
+          enforce: true,
+        },
+      }
+    }
+    return config
+  },
+}
